95d4f477f71e60bcd08de96da5bc3c57e9abaef5
Arunkumar032024 <arunkumar032024@gmail.com>
2024-06-23 14:40:30 +0530

    Add Header components

---
+++ assets/components/Header.js
@@ -0,0 +1,19 @@
+export const Header = `
+<div class="title-container">
+    <img src="./assets/images/logo.png" class="title-img" />
+    <h1>ShoppingMall</h1>
+</div>
+<div class="social-icons-container">
+    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+</div>
+<div class="cart-icon-container">
+    <i class="fa-solid fa-cart-shopping"></i>
+    <span id="cartQuantity">0</span>
+</div>
+<div class="list-icon-container">
+    <i class="fa-solid fa-bars"></i>
+</div>
+`
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+import {Header} from "../components/Header.js"
+
 const apiUrl = 'https://dummyjson.com/products/search?q=shirt';
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    // ============================
+    // add header section 
+    // ============================
+
+    const header = document.querySelector('header');
+    header.innerHTML = Header;
+
+})
