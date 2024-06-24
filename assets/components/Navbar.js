140a5cd9fe1a62eb347a70ba76067e09807a7d21
Arunkumar032024 <arunkumar032024@gmail.com>
2024-06-24 10:06:07 +0530

    Add footer

---
+++ assets/components/Navbar.js
@@ -0,0 +1,10 @@
+export const Navbar = `
+<ul>
+    <li><a href="#">Shop All</a></li>
+    <li><a href="#">Our Story</a></li>
+    <li><a href="#">Our Craft</a></li>
+    <li><a href="#">Gift Card</a></li>
+    <li><a href="#">Contact Us</a></li>
+    <li><a href="#">Login</a></li>
+</ul>
+`
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import {Header} from "../components/Header.js"
+import {Navbar} from "../components/Navbar.js"
 
 const apiUrl = 'https://dummyjson.com/products/search?q=shirt';
 
@@ -11,5 +12,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const header = document.querySelector('header');
     header.innerHTML = Header;
+
+
+    // ============================
+    // add navbar section 
+    // ============================
+    const navbar = document.querySelector('nav');
+    navbar.innerHTML = Navbar;
 
 })
